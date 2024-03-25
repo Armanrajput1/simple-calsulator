d1ad224503cf9f073abbd2783e02cb3b0f2cc046
Armanrajput1 <161529629+Armanrajput1@users.noreply.github.com>
2024-03-26 03:05:50 +0500

    simple calculator
    simple calculator

---
+++ index.js
@@ -0,0 +1,26 @@
+//import inquirer from "inquirer";
+export {};
+//const answer = await inquirer.prompt([
+//{ massage: "Enter First Number", type: "number", name: "Fristnumber" },
+//{ massage: "Enter second number", type: "number", name: "secondnumber" },
+//{
+//  massage: "select one of the operators to perform aperator",
+// type: "list",
+//name: "operator",
+//choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+// },
+//]);
+//conditional statement
+//if (answer.operator === "Addition") {
+//console.log(answer.Fristnumber + answer.secondnumber);
+//} else if (answer.operator === "Subtraction") {
+//console.log(answer.Fristnumber - answer.secondnumber);
+//}
+//else if (answer.operator === "Multiplication") {
+//console.log(answer.Fristnumber * answer.secondnumber);
+//}
+//else if (answer.operator === "Division") {
+//console.log(answer.Fristnumber / answer.secondnumber);
+//} else{
+//console.log("Please select valid operator")
+//}
